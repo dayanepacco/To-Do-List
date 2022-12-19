18ff38856ee4061fe5e2eab3ace71e802a92b87d
dayane pacco <dayane_fs@yahoo.com.br>
2022-12-19 16:28:57 -0300

    add and delete item

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoInputAddItensComponent implements OnInit {
 
+  //ele vai emitir para os outros compoenentes sempre que tiver um event/emit
+  @Output() public emitItemTaskList = new EventEmitter();
+
+  public addItemTaskList: string ="";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  //para add itens
+  public submitItemTaskList(){
+    this.addItemTaskList = this.addItemTaskList.trim();  //trim remove espaço de frente e de trás
+    
+    if(this.addItemTaskList){             //se nao tiver vazio 
+    this.emitItemTaskList.emit(this.addItemTaskList);
+    this.addItemTaskList = "";
+    }
   }
 
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+//Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
@@ -7,11 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
-  
+  public taskList: Array<TaskList> = [
+    { task: "Minha novfa Task", checked: true },
+    { task: "Minha novfa Task 2", checked: false },
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+    
+ //add item
+  public setEmitTaskList(event: string){
+    this.taskList.push({ task: event, checked: false});
 
+  }
+   //deletar cada item
+  public deleteItemTaskList(event: number) {    
+    this.taskList.splice(event, 1);
+  }
+
+  //deletar todos itens da lista de uma vez
+  public deleteAllTaskList(){
+  //janela de aviso se quer mesmo deletar
+  const confirm = window.confirm("você deseja realmente deletar tudo?");
+
+  //se sim apagar tudo
+    if(confirm){
+    this.taskList = [];
+  }
+}
 }
